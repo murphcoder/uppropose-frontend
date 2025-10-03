6a09b8a674eb9f9afa6408bde9413fc75ecbf357
murphcoder <sean@murphcoder.com>
2025-10-03 03:06:22 -0500

    frame adjustments

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'ef52fe5cddc8.ngrok-free.app'
+    ],
+    host: true,   // optional, makes Vite listen on all addresses
+    port: 5173    // or whatever port you use
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true
